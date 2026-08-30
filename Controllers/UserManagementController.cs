a7b42d91873487cc9bf97dd56ef61356dcc6537f
EJ Ridge Lerios <jrdglrs2003@gmail.com>
2026-08-30 16:26:39 +0800

    fix: resolve build errors after pull - clean orphaned migrations, fix connection string to LocalDB, untrack bin artifacts, fix duplicate using

--- Controllers/UserManagementController.cs
+++ Controllers/UserManagementController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
-using System.Security.Claims;
 
 namespace AriesMagicAppointmentSystem.Controllers
 {
--- Migrations/20260830082434_InitialIdentitySquashed.cs
+++ Migrations/20260830082434_InitialIdentitySquashed.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace AriesMagicAppointmentSystem.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialIdentity : Migration
+    public partial class InitialIdentitySquashed : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
